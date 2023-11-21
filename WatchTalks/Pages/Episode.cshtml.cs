456225dcd60e244a17865e3f53cda6a8564fca41
husarmario99 <56115432+husarmario99@users.noreply.github.com>
2023-11-21 15:41:48 +0100

    Vytvorenie TVSeries a Episode pages

---
+++ WatchTalks/Pages/Episode.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WatchTalks.Pages;
+
+public class Episode : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ WatchTalks/Pages/TVSeries.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WatchTalks.Pages;
+
+public class TVSeries : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
